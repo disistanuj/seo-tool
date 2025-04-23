f0bae8eaaf45aa5c66bde30d9946ab809a05cc6d
disistanuj <disistanuj@gmail.com>
2025-04-23 18:32:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function loadTool(toolName) {
-    document.getElementById('tool-frame').src = 'tools/' + toolName + '.html';
-    document.getElementById('tool-frame').style.display = 'block';
-}
+
